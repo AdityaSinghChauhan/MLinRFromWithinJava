f8e09d2bc4a00d29dd10f0d76db61d2a4cc922cc
Aditya Singh Chauhan <astroadityachauhan@gmail.com>
2018-11-22 23:14:43 +0530

    Added method to delete directory and its content

--- src/Utility/Utility.java
+++ src/Utility/Utility.java
@@ -1,6 +1,12 @@
 package Utility;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
-
 
 public class Utility {
 	public static boolean isNullOrBlank(String str)
@@ -10,6 +16,29 @@ public class Utility {
     		return true;
     	}
     	return false;
+    }
+	
+
+	public static boolean deleteDir(String path){
+    	Path directory = Paths.get(path);
+        try {
+			Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+			   @Override
+			   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			       Files.delete(file);
+			       return FileVisitResult.CONTINUE;
+			   }
+
+			   @Override
+			   public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+			       Files.delete(dir);
+			       return FileVisitResult.CONTINUE;
+			   }
+			});
+		} catch (IOException e) {
+			return false;
+		}
+        return true;
     }
     
 }
